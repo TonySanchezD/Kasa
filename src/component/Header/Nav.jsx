7ef8734f050c01b8cb9323c98ed22e1d9ff26016
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-11-24 16:19:17 +0100

    Header fini

--- src/component/Header/Nav.jsx
+++ src/component/Header/Nav.jsx
@@ -2,9 +2,9 @@ import {Link} from 'react-router-dom'
 
 function Nav() {
     return (
-        <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A Propos</Link>
+        <nav className='nav'>
+            <Link to="/" className='nav--link'>Accueil</Link>
+            <Link to="/about" className='nav--link nav--link__last'>A Propos</Link>
         </nav>
     )
 }
