e422a9891a75648a1289dc12e54681a13846e86a
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-11-30 13:32:10 +0100

    Refactorisation du Header

--- src/react/component/Header/Header.jsx
+++ src/react/component/Header/Header.jsx
@@ -1,11 +1,16 @@
-import Logo from './Logo'
-import Nav from './Nav'
+import logo from '../../../layout/logo.png'
+import {Link} from 'react-router-dom'
 
 function Header() {
     return (
         <header className='header'>
-            <Logo />
-            <Nav />
+            <h1>
+                <img src={logo} alt="Logo Kasa" className='logo'/>
+            </h1>
+            <nav className='nav'>
+                <Link to="/" className='nav--link'>Accueil</Link>
+                <Link to="/about" className='nav--link nav--link__last'>A Propos</Link>
+            </nav>
         </header>
     )
 }
--- src/react/pages/home/Main.jsx
+++ src/react/pages/home/Main.jsx
@@ -4,13 +4,13 @@ function Main() {
     return (
         <main>
             <div className="banner">
-            <div className="banner--filter-black"></div>
-            <h2 className="banner--title">Chez vous, partout et ailleurs</h2>
-            <div className="banner--img"></div>
+                <div className="banner--filter-black"></div>
+                <h2 className="banner--title">Chez vous, partout et ailleurs</h2>
+                <div className="banner--img"></div>
             </div>
             <div className="rentals">
-            {data.map(rental => (
-                <div className='rental' key={rental.id}>
+                {data.map(rental => (
+                    <div className='rental' key={rental.id}>
                     <div className='rental--filtre'></div>
                     <img className='rental--img' src={rental.cover} alt={rental.title} />
                     <h3 className='rental--title'>{rental.title}</h3>
