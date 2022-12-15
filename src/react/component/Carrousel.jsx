a0e7860b11798515543e754b0452ef6b2c3989cb
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-12-15 14:52:56 +0100

    Carousel fonctionnel

--- src/react/component/Carrousel.jsx
+++ src/react/component/Carrousel.jsx
@@ -7,17 +7,26 @@ import arrowRight from '../../layout/arrow-right.png';
 function Carrousel(props) {
     const housing = props.housing
 
-    const [positionImg, setImgPosition] = useState(0)
+    //<On défini l'image a afficher dans la carrousel>
+    let [positionImg, setImgPosition] = useState(0)         
+                                                            
+    if (positionImg === -1) {                               
+        setImgPosition(housing.pictures.length - 1)         
+    } else if (positionImg === housing.pictures.length) {   
+        setImgPosition(0)                                   
+    }
+
     const imgCarrousel = housing.pictures[positionImg]
+    //</On défini l'image a afficher dans la carrousel> 
 
     return (
         <div className='carrousel'>
             <img className='carrousel--img' src={imgCarrousel} alt={housing} />
-            <button onClick={() => setImgPosition(positionImg - 1)}>
+            <button className='carrousel--backgroun-arrow carrousel--backgroun-arrow__left' onClick={() => setImgPosition(positionImg - 1)}>
                 <img className='carrousel--arrow-left'  src={arrowLeft} alt='arrow left' />
             </button>
-            <button>
-                <img className='carrousel--arrow-right' onClick={() => setImgPosition(positionImg + 1)} src={arrowRight} alt='arrow right' />
+            <button className='carrousel--backgroun-arrow carrousel--backgroun-arrow__right' onClick={() => setImgPosition(positionImg + 1)}>
+                <img className='carrousel--arrow-right'  src={arrowRight} alt='arrow right' />
             </button>
             <p>{positionImg}</p>
         </div>
