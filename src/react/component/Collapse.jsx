1687ea29f166fbbc9498988aafc6d89f73df5907
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-12-20 15:41:12 +0100

    Changement du composant Collapse pour les description qui doivent être affiché en liste + petit changement sur le padding-bottom du header

--- src/react/component/Collapse.jsx
+++ src/react/component/Collapse.jsx
@@ -4,11 +4,39 @@ import arrowDown from '../../layout/vector_arrow-down.png'
 
 function Collapse(props) {
     const title = props.title
-    const description = props.description
+    let description = props.description
     const index = props.index
 
+    console.log("typeof description", typeof description)
+
     const [isOpen, setIsOpen] = useState(false)
-    
+
+
+    if (typeof description == "object") {
+        return isOpen ? (   //Collapse ouverte
+            <div className="collapse" onClick={() => setIsOpen(false)}>
+                <div className="collapse--to-activate" key={`${title}-${index}-title`}>
+                    <h2 className="collapse--to-activate--title">{title}</h2>
+                    <img className="collapse--to-activate--arrow"src={arrowUp} alt='arrow' />
+                </div>
+                <div className="collapse--description" key={`${title}-${index}-decription`}>
+                    <ul>
+                        {description.map( element => (
+                            <li key={element}>{element}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        ) : (   //Collapse fermé 
+            <div className="collapse" onClick={() => setIsOpen(true)}>
+                <div className="collapse--to-activate" key={`${title}-${index}-title`}>
+                    <h2 className="collapse--to-activate--title">{title}</h2>
+                    <img className="collapse--to-activate--arrow"src={arrowDown} alt='arrow' />
+                </div>
+            </div>
+        )
+    }
+
     return isOpen ? (   //Collapse ouverte
         <div className="collapse" onClick={() => setIsOpen(false)}>
             <div className="collapse--to-activate" key={`${title}-${index}-title`}>
@@ -27,6 +55,8 @@ function Collapse(props) {
             </div>
         </div>
     )
+    
+    
 }
 
 export default Collapse
