be8500bfa405963a648a3d11c9cc2a5e27e3f443
Tony Sanchez <tony-sanchez@hotmail.fr>
2023-06-23 15:42:55 +0200

    Modification du chemin de la home page

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,9 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/kasa" element={<Home />} />
-
                     <Route path="/Housing" element={<Housing/>} />
-                    <Route path="/kasa/Housing" element={<Housing/>} />
-
                     <Route path="/About" element={<About/>} />
-                    <Route path="/kasa/About" element={<About/>} />
-
                     <Route path="/*" element={<Error/>} />
-                    <Route path="/kasa/*" element={<Error/>} />
-
                 </Routes>
             </BrowserRouter>
         </div>
--- src/react/pages/housing/Housing.jsx
+++ src/react/pages/housing/Housing.jsx
@@ -22,6 +22,7 @@ function Housing() {
   const housing = getDataHousing()
 
   if (housing === undefined) {
+      console.log("housing is undefined")
       return <Error />
   }
 
